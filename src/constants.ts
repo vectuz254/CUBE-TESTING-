d14d85abf9dad9ce6da55499c46f1751ebd0df75
vectuz254 <quintineluke16@gmail.com>
2026-09-24 12:08:32 +0100

    Create constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const gradientStyle: React.CSSProperties = {
+  backgroundImage: 'linear-gradient(to right, #091020 0%, #0B2551 12.5%, #A4F4FD 32.5%, #00d2ff 50%, #0B2551 67.5%, #091020 87.5%, #091020 100%)',
+  backgroundSize: '200% auto',
+  WebkitBackgroundClip: 'text',
+  backgroundClip: 'text',
+  color: 'transparent',
+  WebkitTextFillColor: 'transparent',
+  filter: 'url(#c3-noise)',
+};
